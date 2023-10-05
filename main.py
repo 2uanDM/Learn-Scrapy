b89351c6515ee7e35eec1468e55937cd777cda7a
2uanDM <hokage321xxx@gmail.com>
2023-10-05 11:06:27 +0700

    update

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 import subprocess
 
-def run(spider_name: str, export_file: str, overwrite: bool = False):
+def run(spider_name: str, **kwargs):
     # Define the command as a list of strings
     command = ["cd", "src", "&&", "scrapy", "crawl", spider_name]
 
-    if overwrite:
-        options = ["-o",f"../results/{export_file}"]
-    else:
-        options = ["--nolog","-o",f"../results/{export_file}","-a","dont_overwrite=True"]
+    # Add the arguments
+    if kwargs.get('export_dir'):
+        if kwargs.get('overwrite'):
+            command += ["-o", f"../results/{kwargs['export_dir']}", "-a", "dont_overwrite=False"]
+        else:
+            command += ["-o", f"../results/{kwargs['export_dir']}", "-a", "dont_overwrite=True"]
+       
+    if kwargs.get('nolog'):
+        command += ["--loglevel=ERROR"]
 
     # Use subprocess to run the command
-    subprocess.run(command + options, shell=True)
+    subprocess.run(command, shell=True)
 
 if __name__ == '__main__':
-    run(spider_name='quotes', export_file='test.csv',overwrite=True)
+    run(spider_name='quotes')
