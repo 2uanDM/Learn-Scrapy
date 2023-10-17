7d740684d208fc8f17cfcae2121661910a9748e3
2uanDM <hokage321xxx@gmail.com>
2023-10-17 21:48:35 +0700

    run day 17

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ if __name__=='__main__':
     ls_cafef_thread.join()
     ls_lnh_thread.join()
     
-    ls_nhtm.run() # This one cannot be run in a thread since it use tabula to parse pdf (jvm cannot be run in a thread)
+    # ls_nhtm.run() # This one cannot be run in a thread since it use tabula to parse pdf (jvm cannot be run in a thread)
     
     print('All threads finished their jobs')
     sys.exit(0)
