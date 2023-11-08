6f715bd3ffd36b16793afb929c9996002be5bd9e
Duong Minh Quan <91663460+2uanDM@users.noreply.github.com>
2023-11-08 19:21:26 +0700

    delete files

--- src/utils/io.py
+++ src/utils/io.py
@@ -1,26 +1,28 @@
 import csv
 
+
 def flatten_dict(d: dict) -> list:
     '''
         For example d = {'a': 1, 'b': {'c': 2, 'd': 3}}
-        
+
         then the output is [1, 2, 3]
     '''
     output = []
-    
+
     for key in d.keys():
         if isinstance(d[key], dict):
             output.extend(flatten_dict(d[key]))
         else:
             output.append(d[key])
-        
+
     return output
+
 
 def write_csv(file_name: str, data, mode: str = 'a') -> None:
     '''
         data can be a dict or a list of dict
     '''
-    
+
     if isinstance(data, dict):
         # Flatten dict and write to csv file
         with open(file_name, mode, newline='', encoding='utf-8') as f:
@@ -34,16 +36,3 @@ def write_csv(file_name: str, data, mode: str = 'a') -> None:
                 writer.writerow(flatten_dict(d))
     else:
         raise ValueError('Data must be a dict or a list of dict')
-
-# if __name__ == '__main__':
-#     data = {
-#         'a': 1,
-#         'b': {
-#             'c': 2,
-#             'd': 3
-#         }
-#     }
-    
-    
-#     write_csv(r'C:\Users\Thinkbook 14 G3 ACL\Documents\GitHub\Kaxim-Stocks-Topic-2\results\exchange_rate.csv', data)
-    
